449a1eb0b916b5bec657c1018a62ac098d6714bf
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-08-20 20:01:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,24 @@
+# main.tf
 provider "google" {
-    project     = "tf-gcp-connection"
+  credentials = "2d4bb0e3165672956155143594bacfc52c597322"
+  project     = "tf-gcp-connection"
   region      = "us-central1"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "my-vpc"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "subnetwork" {
-  name          = "my-subnetwork"
-  ip_cidr_range = "10.0.0.0/24"
-  network       = google_compute_network.vpc_network.id
-  region        = "us-central1"
-}
-
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-vm"
-  machine_type  = "e2-medium"
+  name         = "test-instance"
+  machine_type = "e2-micro"
   zone         = "us-central1-a"
-  
+
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10-buster-v20210817"
+      image = "debian-cloud/debian-9"
     }
   }
-  
+
   network_interface {
-    network    = google_compute_network.vpc_network.id
-    subnetwork = google_compute_subnetwork.subnetwork.id
-
+    network = "default"
     access_config {
-      // Assign a public IP
     }
   }
-  
-  tags = ["web"]
-}
-
-resource "google_storage_bucket" "bucket" {
-  name          = "terraform-bucket-unique-name"
-  location      = "US"
 }
